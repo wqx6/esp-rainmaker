18829ece5df4582152129849fa3781da1a0bc49a
WanqQixiang <wangqixiang@espressif.com>
2024-02-05 16:03:02 +0800

    examples: Add client-only matter controller example

---
+++ examples/matter/client_only_matter_controller/main/rmaker_controller_service/app_matter_controller_callback.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <app_matter_controller.h>
+
+esp_err_t app_matter_controller_callback(matter_controller_handle_t *handle, matter_controller_callback_type_t type);
---
+++ examples/matter/client_only_matter_controller/main/rmaker_controller_service/app_matter_device_manager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <esp_matter_console.h>
+#include <matter_device.h>
+#include <stdint.h>
+
+#include <app_matter_controller.h>
+
+namespace esp_matter {
+namespace console {
+
+/** Add controller device manager commands
+ *
+ * Add the default controller device manager commands
+ *
+ * @return ESP_OK on success
+ * @return error in case of failure
+ */
+esp_err_t ctl_dev_mgr_register_commands();
+
+} // namespace console
+} // namespace esp_matter
+
+typedef void (*device_list_update_callback_t)(void);
+
+esp_err_t update_device_list(matter_controller_handle_t *controller_handle);
+
+matter_device_t *fetch_device_list();
+
+esp_err_t init_device_manager(device_list_update_callback_t dev_list_update_cb);
